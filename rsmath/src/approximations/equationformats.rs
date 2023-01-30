1ec0c67ca1c334c2c44c37b9519c829b34ab2ffe
nsong03 <85597680+nsong03@users.noreply.github.com>
2023-01-30 11:42:27 -0800

    1.30.2023 update

---
+++ rsmath/src/approximations/equationformats.rs
@@ -0,0 +1,1 @@
+// Defines allowed inputs / formulas / equations. 
---
+++ rsmath/src/approximations/integrationmethods.rs
@@ -0,0 +1,7 @@
+use crate::math::{Real, Vector};
+use nalgebra::
+
+pub struct 
+pub fn newtons_method(x0: i32, f: &dyn FnMut(i32) -> i32) -> i32 {
+    
+}
